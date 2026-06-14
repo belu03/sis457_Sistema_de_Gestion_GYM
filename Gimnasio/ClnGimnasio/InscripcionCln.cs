55d7ff3c3299d48a582ec31e9fef6c50143e6c5c
belenct3@gmail.com <belenct3@gmail.com>
2026-06-13 22:38:09 -0400

    cambiando HorarioClaseCln, ReporteCln y ReservaCln

---
+++ Gimnasio/ClnGimnasio/InscripcionCln.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CadGimnasio;
+
+
+namespace ClnGimnasio
+{
+    public class InscripcionCln
+    {
+        public static int crear(Inscripcion inscripcion)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                context.Inscripcion.Add(inscripcion);
+                context.SaveChanges();
+                return inscripcion.id;
+            }
+        }
+
+        public static int anular(int id, string usuarioRegistro)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                var existente = context.Inscripcion.Find(id);
+                if (existente != null)
+                {
+                    existente.estado_inscripcion = "Anulada";
+                    existente.estado = 0;
+                    existente.usuarioRegistro = usuarioRegistro;
+                    return context.SaveChanges();
+                }
+                return 0;
+            }
+        }
+
+        public static List<paInscripcionListar_Result> listarPa(string parametro)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                return context.paInscripcionListar(parametro.Trim()).ToList();
+            }
+        }
+    }
+}
---
+++ Gimnasio/ClnGimnasio/ReservaCln.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CadGimnasio;
+using System.Threading.Tasks;
+
+namespace ClnGimnasio
+{
+    public class ReservaCln
+    {
+        public static int crear(Reserva reserva)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var horario = context.HorarioClase.Find(reserva.id_horarioclase);
+                        var servicio = context.Servicio.Find(horario.id_servicio);
+
+                        int cuposActuales = horario.cupos_reservados;
+
+                        if (cuposActuales >= servicio.capacidad_maxima)
+                        {
+                            throw new Exception("La clase ha alcanzado su capacidad máxima.");
+                        }
+
+                        horario.cupos_reservados = cuposActuales + 1;
+                        context.Reserva.Add(reserva);
+
+                        context.SaveChanges();
+                        transaction.Commit();
+                        return reserva.id;
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public static int modificar(Reserva reserva)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                var existente = context.Reserva.Find(reserva.id);
+                if (existente != null)
+                {
+                    existente.id_cliente = reserva.id_cliente;
+                    existente.id_horarioclase = reserva.id_horarioclase;
+                    existente.fecha_reserva = reserva.fecha_reserva;
+                    existente.asistio = reserva.asistio;
+                    existente.estado_reserva = reserva.estado_reserva;
+                    existente.usuarioRegistro = reserva.usuarioRegistro;
+                    return context.SaveChanges();
+                }
+                return 0;
+            }
+        }
+
+        public static int eliminar(int id, string usuarioRegistro)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var reserva = context.Reserva.Find(id);
+                        if (reserva != null && reserva.estado != 0)
+                        {
+                            reserva.estado = 0;
+                            reserva.estado_reserva = "Anulada";
+                            reserva.usuarioRegistro = usuarioRegistro;
+
+                            var horario = context.HorarioClase.Find(reserva.id_horarioclase);
+                            if (horario != null && horario.cupos_reservados > 0)
+                            {
+                                horario.cupos_reservados -= 1;
+                            }
+
+                            context.SaveChanges();
+                            transaction.Commit();
+                            return 1;
+                        }
+                        return 0;
+                    }
+                    catch (System.Exception)
+                    {
+                        transaction.Rollback();
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        public static Reserva obtenerUno(int id)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                return context.Reserva.Find(id);
+            }
+        }
+
+        public static List<paReservaListar_Result> listarPa(string parametro)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                return context.paReservaListar(parametro.Trim()).Where(x => x.estado == 1).ToList();
+            }
+        }
+    }
+}

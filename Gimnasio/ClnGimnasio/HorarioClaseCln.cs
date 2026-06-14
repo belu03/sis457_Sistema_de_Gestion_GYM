775f508b22a2f312ac6257180bad93f0278d33fb
belenct3@gmail.com <belenct3@gmail.com>
2026-06-13 21:27:30 -0400

    actualizacion

---
+++ Gimnasio/ClnGimnasio/HorarioClaseCln.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CadGimnasio;
+
+namespace ClnGimnasio
+{
+    public class HorarioClaseCln
+    {
+        public static int crear(HorarioClase horario)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                context.HorarioClase.Add(horario);
+                context.SaveChanges();
+                return horario.id;
+            }
+        }
+
+        public static int modificar(HorarioClase horario)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                var existente = context.HorarioClase.Find(horario.id);
+                if (existente != null)
+                {
+                    existente.id_servicio = horario.id_servicio;
+                    existente.id_entrenador = horario.id_entrenador;
+                    existente.dia_semana = horario.dia_semana;
+                    existente.hora_inicio = horario.hora_inicio;
+                    existente.hora_fin = horario.hora_fin;
+                    existente.usuarioRegistro = horario.usuarioRegistro;
+                    return context.SaveChanges();
+                }
+                return 0;
+            }
+        }
+
+        public static int eliminar(int id, string usuarioRegistro)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                var existente = context.HorarioClase.Find(id);
+                if (existente != null)
+                {
+                    existente.estado = 0;
+                    existente.usuarioRegistro = usuarioRegistro;
+                    return context.SaveChanges();
+                }
+                return 0;
+            }
+        }
+
+        public static HorarioClase obtenerUno(int id)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                return context.HorarioClase.Find(id);
+            }
+        }
+
+        public static List<HorarioClase> listar()
+        {
+            using (var context = new GimnasioEntities())
+            {
+                return context.HorarioClase.Where(x => x.estado == 1).ToList();
+            }
+        }
+
+        public static List<paHorarioClaseListar_Result> listarPa(string parametro)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                return context.paHorarioClaseListar(parametro.Trim()).ToList();
+            }
+        }
+    }
+}
---
+++ Gimnasio/ClnGimnasio/ReporteCln.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CadGimnasio;
+
+namespace ClnGimnasio
+{
+    public class ReporteCln
+    {
+
+        public static List<paReporteIngresos_Result> ReporteIngresos(DateTime inicio, DateTime fin)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                return context.paReporteIngresos(inicio.Date, fin.Date).ToList();
+            }
+        }
+
+
+        public class MembresiaPopularResult
+        {
+            public int IdMembresia { get; set; }
+            public int CantidadVendida { get; set; }
+            public decimal TotalIngresos { get; set; }
+        }
+
+        public static List<MembresiaPopularResult> ObtenerMembresiasPopulares()
+        {
+            using (var context = new GimnasioEntities())
+            {
+                return context.Inscripcion
+                    .Where(i => i.estado == 1 && i.estado_inscripcion == "Activa")
+                    .GroupBy(i => i.id_membresia)
+                    .Select(g => new MembresiaPopularResult
+                    {
+                        IdMembresia = g.Key,
+                        CantidadVendida = g.Count(),
+                        TotalIngresos = g.Sum(i => i.monto_pagado)
+                    })
+                    .OrderByDescending(r => r.CantidadVendida)
+                    .ToList();
+            }
+        }
+
+
+        public class AlertaVencimientoResult
+        {
+            public int IdCliente { get; set; }
+            public int IdMembresia { get; set; }
+            public DateTime Vencimiento { get; set; }
+            public int DiasRestantes { get; set; }
+        }
+
+        public static List<AlertaVencimientoResult> ObtenerAlertaVencimientos(int diasMargen = 7)
+        {
+            using (var context = new GimnasioEntities())
+            {
+                DateTime fechaLimite = DateTime.Now.Date.AddDays(diasMargen);
+                DateTime hoy = DateTime.Now.Date;
+
+                var vencimientos = context.Inscripcion
+                    .Where(i => i.estado == 1 && i.estado_inscripcion == "Activa" && i.fecha_fin >= hoy && i.fecha_fin <= fechaLimite)
+                    .ToList();
+
+                return vencimientos.Select(i => new AlertaVencimientoResult
+                {
+                    IdCliente = i.id_cliente,
+                    IdMembresia = i.id_membresia,
+                    Vencimiento = i.fecha_fin,
+                    DiasRestantes = (i.fecha_fin.Date - hoy).Days
+                })
+                    .OrderBy(r => r.DiasRestantes)
+                    .ToList();
+            }
+        }
+    }
+}
